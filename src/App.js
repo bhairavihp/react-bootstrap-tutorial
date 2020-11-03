bc4917b082a6942af0ebc2cf2b902c3324cf1c36
bhairavihp <57017162+bhairavihp@users.noreply.github.com>
2020-11-03 18:24:05 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container,Row,Col,Button,Alert,Breadcrumb,Card, Form } from 'react-bootstrap'
+import {Container,Row,Col, Button,Alert,Breadcrumb,Card, Form } from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
